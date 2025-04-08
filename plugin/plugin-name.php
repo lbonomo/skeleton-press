40f357b40e71658b2d40925eb33d0d7004d170ba
Lucas Bonomo <bonomo.lucas@gmail.com>
2025-04-08 10:18:19 -0300

    Adding php file

---
+++ plugin/plugin-name.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Plugin Name
+ * Plugin URI: https://lucasbonomo.com/wordpress/plugins/plugin-name
+ * Description: A brief description of what the plugin does.
+ * Version: 1.0.0
+ * Author: Lucas Bonomo
+ * Author URI: https://lucasbonomo.com/wordpress/plugins
+ * License: GPL2
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
